0d7e4f0f35b06e83f22a8df2e57a1cba459d3c0f
Parth Patil <parth253pp@outlook.com>
2025-02-11 08:19:12 +0530

    without cors

--- src/main/java/com/email/controller/EmailController.java
+++ src/main/java/com/email/controller/EmailController.java
@@ -8,8 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
+// @CrossOrigin(origins="https://emailgui.vercel.app")
 @RestController
-@CrossOrigin(origins="https://emailgui.vercel.app")
 public class EmailController {
 
 
