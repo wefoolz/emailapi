1677955cc1679217216ba13e90de857715c475c9
Parth Patil <parth253pp@outlook.com>
2025-02-11 08:01:48 +0530

    perticular cross origin

--- src/main/java/com/email/controller/EmailController.java
+++ src/main/java/com/email/controller/EmailController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins="*")
+@CrossOrigin(origins="https://emailgui.vercel.app")
 public class EmailController {
 
 
